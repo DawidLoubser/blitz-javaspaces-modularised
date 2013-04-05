05883d7f2d6a1afc2ce216f34959801ec1fdbd63
dreedyman <dennis.reedy@gmail.com>
2013-04-05 13:39:12 -0400

    Moved test case to blitz-service module

--- src/test/conf/test-config.groovy
+++
@@ -1,9 +0,0 @@
-ITBlitzRioDeployment
-{
-    groups = System.getProperty('user.name') + ".test.ITBlitzRioDeployment"
-    numCybernodes = 1
-    numMonitors = 1
-    opstring = 'src/test/conf/deployment.groovy'
-    autoDeploy = true
-}
-
