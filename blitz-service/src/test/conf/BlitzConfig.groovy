fff1c0cd831c16a92d3206dfcc5d25e3cc425d80
dreedyman <dennis.reedy@gmail.com>
2013-04-05 15:43:17 -0400

    Added configurations and resources

---
+++ blitz-service/src/test/conf/BlitzConfig.groovy
@@ -0,0 +1,67 @@
+import net.jini.export.Exporter
+import net.jini.jeri.BasicJeriExporter
+import net.jini.jeri.ProxyTrustILFactory
+import net.jini.jeri.tcp.TcpServerEndpoint
+import net.jini.security.BasicProxyPreparer
+import net.jini.security.ProxyPreparer
+import org.dancres.blitz.config.Transient
+import org.dancres.blitz.stats.InstanceSwitch
+import org.dancres.blitz.stats.OpSwitch
+import org.dancres.blitz.stats.Switch
+import org.rioproject.config.Component
+
+@Component('org.dancres.blitz')
+class BlitzConfig {
+    boolean syncNotifyOnWrite = true;
+    boolean ignoreLogConfig = true
+
+    String name = "Space";
+    String persistDir = "${System.getProperty("user.dir")}/target/blitz/data"
+    String logDir = "${System.getProperty("user.dir")}/target/blitz/log";
+
+    int maxWriteThreads = 1
+    int desiredPendingWrites = 256
+    long dbCache = 27262976
+    int maxDbTxns = 256
+    boolean dumpDbStats = false
+    boolean logCkpts = false
+    int entryReposCacheSize = 4096;
+    int cacheEntriesPerPartition = 32
+
+    def getLoadBackoff() {
+        def loadBackoff = [20,50]
+        return loadBackoff as int[]
+    }
+
+    long entryLeaseBound = 0;
+    int notifyLeaseBound = 0;
+    long leaseReapInterval = 0;
+    int maxOidAllocators = 512;
+    int eventgenSaveInterval = 500;
+    int eventgenRestartJump = 1000;
+    int maxTaskThreads = 8;
+    int maxEventProcessors = 4;
+
+    // TODO: Configure TimeBarrierPersistent/Persistent
+    Transient storageModel = new Transient();
+
+    long statsDump = 60000;
+    boolean compliantDestroy = false
+
+    def getStats() {
+        def stats = [new OpSwitch(OpSwitch.ALL_TYPES, OpSwitch.TAKE_OPS, true),
+                     new OpSwitch(OpSwitch.ALL_TYPES, OpSwitch.READ_OPS, true),
+                     new OpSwitch(OpSwitch.ALL_TYPES, OpSwitch.WRITE_OPS, true),
+                     new InstanceSwitch(InstanceSwitch.ALL_TYPES, true)]
+        return stats as Switch[]
+    }
+
+    Exporter serverExporter = new BasicJeriExporter(TcpServerEndpoint.getInstance(0),
+                                                     new ProxyTrustILFactory(null, null), false, true);
+    ProxyPreparer notifyPreparer = new BasicProxyPreparer();
+    ProxyPreparer recoveredNotifyPreparer = new BasicProxyPreparer();
+    ProxyPreparer txnPreparer = new BasicProxyPreparer();
+    ProxyPreparer recoveredTxnPreparer = new BasicProxyPreparer();
+    ProxyPreparer activationIdPreparer = new BasicProxyPreparer();
+    ProxyPreparer activationSysPreparer = new BasicProxyPreparer()
+}
--- blitz-service/src/test/conf/deployment.groovy
+++ blitz-service/src/test/conf/deployment.groovy
@@ -4,8 +4,7 @@ import java.util.logging.Level
 /** Most basic example of deploying a default Blitz instance.
  * See Blitz documentation for configuration options.
  */
-deployment(name:'Basic Blitz Deployment')
-{
+deployment(name:'Basic Blitz Deployment') {
     /* Configuration for the discovery group that the services should join */
     groups System.getProperty(Constants.GROUPS_PROPERTY_NAME, System.getProperty('user.name'))
 
@@ -13,9 +12,7 @@ deployment(name:'Basic Blitz Deployment')
     artifact id: 'blitz-dl',   'org.dancres:blitz-proxy:2.1.7'
     artifact id: 'blitz-impl', 'org.dancres:blitz-service:2.1.7'
 
-
-    logging
-    {
+    logging {
       logger 'com.travellinck', Level.INFO
     }
 
@@ -34,63 +31,8 @@ deployment(name:'Basic Blitz Deployment')
 
         // Sample configuration for a basic, untuned, default instance. See Blitz
         // sample configs for details
-        configuration '''
-        import net.jini.security.BasicProxyPreparer;
-        import net.jini.jeri.tcp.TcpServerEndpoint;
-        import net.jini.jeri.ProxyTrustILFactory;
-        import net.jini.jeri.BasicJeriExporter;
-        import org.dancres.blitz.stats.Switch;
-        import org.dancres.blitz.stats.OpSwitch;
-        import org.dancres.blitz.stats.InstanceSwitch;
-        import org.dancres.blitz.config.Transient;
-
-        org.dancres.blitz
-        {
-
-            syncNotifyOnWrite = true;
-            ignoreLogConfig = new Boolean(true);
-            name = "Space";
-            // TODO: Auto-set to somehwere in RIO install dir
-            persistDir = "${RIO_HOME}/system/external/blitz/data";
-            logDir = "${RIO_HOME}/system/external/blitz/log";
-            maxWriteThreads = 1;
-            desiredPendingWrites = 256;
-            dbCache = 27262976;
-            maxDbTxns = 256;
-            dumpDbStats = new Boolean(false);
-            logCkpts = new Boolean(false);
-            entryReposCacheSize = 4096;
-            cacheEntriesPerPartition = 32;
-            loadBackoff = new int[] {20, 50};
-            entryLeaseBound = 0;
-            notifyLeaseBound = 0;
-            leaseReapInterval = 0;
-            maxOidAllocators = 512;
-            eventgenSaveInterval = 500;
-            eventgenRestartJump = 1000;
-            maxTaskThreads = 8;
-            maxEventProcessors = 4;
-
-            // TODO: Configure TimeBarrierPersistent/Persistent
-            storageModel = new Transient();
-
-            statsDump = 60000;
-            compliantDestroy = new Boolean(false);
-            stats = new Switch[] {new OpSwitch(OpSwitch.ALL_TYPES,
-            OpSwitch.TAKE_OPS, true),
-            new OpSwitch(OpSwitch.ALL_TYPES, OpSwitch.READ_OPS, true),
-            new OpSwitch(OpSwitch.ALL_TYPES, OpSwitch.WRITE_OPS, true),
-            new InstanceSwitch(InstanceSwitch.ALL_TYPES, true)};
-            serverExporter = new BasicJeriExporter( TcpServerEndpoint.getInstance(0),
-                                  new ProxyTrustILFactory(null, null), false, true);
-            notifyPreparer = new BasicProxyPreparer();
-            recoveredNotifyPreparer = new BasicProxyPreparer();
-            txnPreparer = new BasicProxyPreparer();
-            recoveredTxnPreparer = new BasicProxyPreparer();
-            activationIdPreparer = new BasicProxyPreparer();
-            activationSysPreparer = new BasicProxyPreparer();
-        }
-        '''
+        //configuration "file:src/test/conf/blitz.config"
+        configuration "file:src/test/conf/BlitzConfig.groovy"
 
         maintain 1
     }
